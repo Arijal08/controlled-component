938ced3780ba50562d523dfb324b2b0bd925c297
Arijal Sidik <107527037+Arijal08@users.noreply.github.com>
2024-11-30 23:42:43 +0700

    Controlled Form

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import ControlledForm from "./assets/components/ControlledForm";
+
+// const element = <p>Arijal Sidik</p>;
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(<ControlledForm />);
